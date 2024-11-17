c5f9b7be69761b79e568e1687d8296c8a535f7d1
rahulbaradol <rahul.baradol.14@gmail.com>
2024-11-18 00:45:25 +0530

    added newline support to terminal

--- simple-kernel/kernel.c
+++ simple-kernel/kernel.c
@@ -72,11 +72,6 @@ void terminal_initialize(void)
 	}
 }
 
-void terminal_setcolor(uint8_t color) 
-{
-	terminal_color = color;
-}
-
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
 {
 	const size_t index = y * VGA_WIDTH + x;
@@ -85,6 +80,13 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 
 void terminal_putchar(char c) 
 {
+	if (c == '\n') {
+		terminal_column = 0;
+		if (++terminal_row == VGA_HEIGHT)
+			terminal_row = 0;
+		return;
+	}
+
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
@@ -109,6 +111,5 @@ void kernel_main(void)
 	/* Initialize terminal interface */
 	terminal_initialize();
 
-	/* Newline support is left as an exercise. */
-	terminal_writestring("Hello, kernel World!\n");
+	terminal_writestring("Bankai!\n\nGetsugatensho...");
 }
